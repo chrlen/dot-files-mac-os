6a9203cb4f78853ea8f3089ed3beca9b264f44c2
Christian Lengert <clengert@eclassifiedsgroup.com>
2022-03-25 17:47:22 +0100

    init

---
+++ scripts/deploy.zsh
@@ -0,0 +1,11 @@
+#! /bin/zsh
+
+echo "vimrc"
+cp src/home/.vimrc ~/.vimrc
+echo "zshrc"
+cp src/home/.zshrc ~/.zshrc
+echo "kitty.conf"
+cp src/home/.config/kitty/kitty.conf  ~/.config/kitty/
+echo "current-theme.conf"
+cp src/home/.config/kitty/current-theme.conf  ~/.config/kitty/
+
